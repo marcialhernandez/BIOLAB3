ad5394a9f31ce8b7a9d5bd10809d31690bf82c41
Marcial Hernandez <marcial.hernandez@usach.cl>
2015-09-26 15:53:30 -0300

    v0.5.1 minimización de codigo

--- main.R
+++ main.R
@@ -12,7 +12,7 @@ inicializaMatriz<-function(largo,ancho){
   return (matrix(0, nrow = largo, ncol = ancho))
 }
 
-generaMatrizMatch2<-function(salidaDeReubicaStrings,bonoMatch,penalizacionMiss,penalizacionGap){
+generaMatrizMatch<-function(salidaDeReubicaStrings,bonoMatch,penalizacionMiss,penalizacionGap){
   #Genero matriz
   matriz=inicializaMatriz(salidaDeReubicaStrings$parametro2$largo+1,salidaDeReubicaStrings$parametro1$largo+1)
   colnames(matriz)<-c("-",salidaDeReubicaStrings$parametro1$valor)
@@ -63,166 +63,6 @@ generaMatrizMatch2<-function(salidaDeReubicaStrings,bonoMatch,penalizacionMiss,p
   }#Fin for (posLetra2 in 1:salidaDeReubicaStrings$parametro2$largo)
   
   return (matriz)
-}
-
-generaMatrizMatch<-function(salidaDeReubicaStrings,bonoMatch,penalizacionMissMatch,penalizacionGap){
-  #Genero matriz
-  matriz=inicializaMatriz(salidaDeReubicaStrings$parametro2$largo,salidaDeReubicaStrings$parametro1$largo)
-  #print (matriz)
-  ejeX<-0
-  ejeY<-0
-  
-  for (posLetra2 in 1:salidaDeReubicaStrings$parametro2$largo){
-    ejeX<-ejeX+1
-    ejeY<-0
-    for (posLetra1 in 1:salidaDeReubicaStrings$parametro1$largo){
-      ejeY<-ejeY+1
-      #Valida correcto cruce de informacion para la comparacion
-      #print (paste("Pos (",toString(ejeX),",",toString(ejeY),")",": Cruce ->",salidaDeReubicaStrings$parametro1$valor[posLetra1],"-",salidaDeReubicaStrings$parametro2$valor[posLetra2]))
-      
-      #-Formacion Matriz----------------------------------------------------------------# 
-      if (ejeX==1){ 
-        
-        if (ejeY==1){ #ejeX==1 && ejeY==1
-          
-          if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-            print
-            matriz[ejeX,ejeY]<-matriz[ejeX,ejeY]+bonoMatch
-          }
-          
-          else{# casoMissMatch
-            matriz[ejeX,ejeY]<-0
-          }
-          
-        }#Fin caso #ejeX==1 && ejeY==1
-        
-        else{ #ejeX==1 && ejeY!=1
-          
-          if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-            matriz[ejeX,ejeY]<-matriz[ejeX,ejeY-1]+bonoMatch
-          }
-          
-          else{# casoMissMatch
-            if (matriz[ejeX,ejeY-1]-penalizacionMissMatch>0){
-              matriz[ejeX,ejeY]<-matriz[ejeX,ejeY-1]-penalizacionMissMatch
-            }
-            else{
-              matriz[ejeX,ejeY]<-0
-            }
-          }
-          
-        }# Fin caso #ejeX==1 && ejeY!=1
-        
-      }
-      
-      else if(ejeY==1){ #ejeX!=1 && ejeY==1
-        
-        if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-          matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY]+bonoMatch
-        }
-        
-        else{# casoMissMatch
-          if (matriz[ejeX-1,ejeY]-penalizacionMissMatch>0){
-            matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY]-penalizacionMissMatch
-          }
-          else{
-            matriz[ejeX,ejeY]<-0
-          }
-        }
-        
-      }#Fin #ejeX!=1 && ejeY==1
-      
-      else{ #ejeX!=1 && ejeY!=1
-        
-        if(matriz[ejeX-1,ejeY-1]>matriz[ejeX-1,ejeY] & matriz[ejeX-1,ejeY-1]>matriz[ejeX,ejeY-1]){ #Caso Cruzado
-          
-          if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-            matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY-1]+bonoMatch
-          }
-          
-          else{# casoMissMatch
-            if (matriz[ejeX-1,ejeY-1]>penalizacionMissMatch){
-              matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY-1]-penalizacionMissMatch
-            }
-            else{
-              matriz[ejeX,ejeY]<-0
-            }
-          }
-          
-        } #Fin Caso Cruzado
-        
-        else if (matriz[ejeX-1,ejeY]>matriz[ejeX-1,ejeY-1] & matriz[ejeX-1,ejeY]>matriz[ejeX,ejeY-1]){ #Caso Izq (Gap)
-          
-          if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-            matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY]-penalizacionGap
-          }
-          
-          else{# casoMissMatch
-            if (matriz[ejeX-1,ejeY]>penalizacionMissMatch){
-              matriz[ejeX,ejeY]<-matriz[ejeX-1,ejeY]-penalizacionMissMatch
-            }
-            else{
-              matriz[ejeX,ejeY]<-0
-            }
-          }
-          
-        } #Fin Caso Izq (Gap)
-        
-        else{ #Caso Arriba (Gap)
-          
-          if(salidaDeReubicaStrings$parametro1$valor[posLetra1]==salidaDeReubicaStrings$parametro2$valor[posLetra2]){ #casoMatch
-            matriz[ejeX,ejeY]<-matriz[ejeX,ejeY-1]-penalizacionGap
-          }
-          
-          else{# casoMissMatch
-            if (matriz[ejeX,ejeY-1]>penalizacionMissMatch){
-              matriz[ejeX,ejeY]<-matriz[ejeX,ejeY-1]-penalizacionMissMatch
-            }
-            else{
-              matriz[ejeX,ejeY]<-0
-            }
-          }
-          
-        }# Fin Caso Arriba (Gap)
-        
-      }#Fin ejeX!=1 && ejeY!=1
-      
-      #---------------------------------------------------------------------------------#
-      
-    } #Fin for (posLetra1 in 1:salidaDeReubicaStrings$parametro1$largo)
-  }#Fin for (posLetra2 in 1:salidaDeReubicaStrings$parametro2$largo)
-  
-  return (matriz)
-}
-
-#Funcion que tiene como entrada 2 parametros de tipo string 
-#Y retorna una lista con los valores 'parametro1' y 'parametro2'
-#Donde siempre el 'parametro1' tendra largo mayor que 'parametro2'
-#tanto parametro1 como parametro2 tienen los atributos 'valor' y 'largo'
-#En caso que una entrada no sea String, entonces retorna el bool FALSE
-reubicaStrings<-function(string1,string2){
-  #Validacion de tipo
-  #En caso que si sea
-  if (typeof(string1)=="character" & typeof(string2)=="character"){
-    
-    #Se asocia su largo como atributo
-    string1.largo=nchar(string1)
-    string2.largo=nchar(string2)
-    
-    if (string1.largo>=string2.largo){
-      #salida.string1=string1
-      #salida.string2=string2
-      return (list(parametro1=list(valor=unlist(strsplit(string1,"")),largo=string1.largo),parametro2=list(valor=unlist(strsplit(string2,"")),largo=string2.largo)))
-    } #fin if (string1.largo>string2.largo)
-    
-    else{
-      return (list(parametro1=list(valor=unlist(strsplit(string2,"")),largo=string2.largo),parametro2=list(valor=unlist(strsplit(string1,"")),largo=string1.largo)))
-    } #fin else
-    
-  } #fin if (typeof(string1)=="character" & typeof(string2)=="character") 
-  else{
-    return(FALSE)
-  }
 }
 
 validaStrings<-function(salidaDeReubicaStrings){
@@ -320,7 +160,7 @@ penalizacionGap<-1
 
 entrada<-reubicaStrings(stringEntrada1,stringEntrada2);
 if (validaStrings(entrada)==TRUE){
-  matrizM=generaMatrizMatch2(entrada,bonoMatch,penalizacionMiss,penalizacionGap)
+  matrizM=generaMatrizMatch(entrada,bonoMatch,penalizacionMiss,penalizacionGap)
   maxPosMatriz<-which(matrizM == max(matrizM), arr.ind = TRUE)
   cantidadMax<-length(maxPosMatriz[,1]) #1 es y, 2 es x
   print(matrizM)
